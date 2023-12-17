cf00a73f5d28ce5956120295be3e473df7ad2340
LeshchuDev <leshchudev@gmail.com>
2023-12-17 20:34:00 +0100

    I puted the API Key into the env local to hide it

--- src/components/Matches.jsx
+++ src/components/Matches.jsx
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from "react";
 export default function Matches() {
   const [match, setMatch] = useState([]);
   const [error, setError] = useState("");
+  const pubgKey = import.meta.env.VITE_APP_PUBG_KEY;
+
 
   useEffect(() => {
     fetch(
@@ -10,7 +12,7 @@ export default function Matches() {
       {
         headers: {
           Authorization: 
-            "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJkNjAwMjQ2MC03ZTI5LTAxM2MtNWRlNC02ZTNmMGZhMTY4N2QiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNzAyNzIxNjc1LCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6Ii0xYWVlOTRmOC04ODUyLTRlYzEtYTNiNS04OGQyODUwNDhlMzIifQ.BzvR6GMxNwg3nP3MkolNMjXEDuLzXLBc8bloXixbdMc",
+          `Bearer ${pubgKey}`,
           Accept: "application/vnd.api+json",
         },
       }
