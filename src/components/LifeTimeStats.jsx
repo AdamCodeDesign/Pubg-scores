19131bc3876d2104ab55c07c8aa0ff539094cb85
LeshchuDev <leshchudev@gmail.com>
2024-01-24 19:15:44 +0100

    Readable seasonsList

--- src/components/LifeTimeStats.jsx
+++ src/components/LifeTimeStats.jsx
@@ -8,11 +8,13 @@ import {
   Box,
   Grid,
   Avatar,
+  MenuItem,
 } from "@mui/material";
 import GameModeStats from "./GameModeStats";
 import { useParams, useNavigate, useLocation } from "react-router-dom";
 
 export default function LifetimeStats() {
+  const pubgKey = import.meta.env.VITE_APP_PUBG_KEY;
   const [playerLifetime, setPlayerLifetime] = useState(null);
   const [error, setError] = useState("");
   const [gameMode, setGameMode] = useState("solo");
@@ -20,7 +22,8 @@ export default function LifetimeStats() {
   const { platformParam } = useParams();
   const location = useLocation();
   const [avatarName, setAvatarName] = useState("");
-  const [season, setSeason] = useState("lifetime")
+  const [seasonsList, setSeasonsList] = useState(null);
+  const [season, setSeason] = useState("lifetime");
   const buttonsGameMode = [
     "solo",
     "duo",
@@ -29,10 +32,36 @@ export default function LifetimeStats() {
     "duo-fpp",
     "squad-fpp",
   ];
-  const pubgKey = import.meta.env.VITE_APP_PUBG_KEY;
-
 
   useEffect(() => {
+    fetch(
+      // `https://api.pubg.com/shards/steam/players/account.61a88fdd49e641408d15e6210e6db699/seasons/division.bro.official.pc-2018-27`,
+      `https://api.pubg.com/shards/steam/seasons`,
+      {
+        headers: {
+          Authorization: `Bearer ${pubgKey}`,
+          Accept: "application/vnd.api+json",
+        },
+      }
+    )
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        return {
+          error: "Cosik poszło nie tak",
+        };
+      })
+      .then((data) => {
+        if (data.error) {
+          setError(data.error);
+          console.log(error);
+        } else {
+          setError("");
+          setSeasonsList(data.data);
+          console.log("Season", data.data);
+        }
+      });
     accountIdParam && platformParam;
     fetch(
       `https://api.pubg.com/shards/${platformParam}/players/${accountIdParam}/seasons/${season}`,
@@ -62,13 +91,39 @@ export default function LifetimeStats() {
           setAvatarName(location.state);
         }
       });
-  }, [accountIdParam, error, pubgKey, platformParam, location.state]);
+  }, [accountIdParam, error, pubgKey, platformParam, location.state, season]);
 
+  const switchSeason = (e) => {
+    e.preventDefault();
+    setSeason(e.target.value);
+  };
+console.log("Lista sezonów",seasonsList)
   return (
     <Stack width="100%">
-      <Typography color="white" fontSize="2em" p={1}>
-        Lifetime Stats
-      </Typography>
+      <Grid item md={3} xs={12} p={1}>
+        <TextField
+          value={season}
+          onChange={switchSeason}
+          select
+          label={season}
+          color="warning"
+          variant="filled"
+          sx={{
+            width: "100%",
+            color: "rgba(170,170,170,0.5)",
+            bgcolor: "rgba(170,170,170,0.5)",
+            fontSize: "1em",
+            border: "1px solid white",
+            borderRadius: "6px",
+          }}
+        >
+          <MenuItem value="xbox">xbox</MenuItem>
+          <MenuItem value="stadia">stadia</MenuItem>
+          <MenuItem value="kakao">kakao</MenuItem>
+          <MenuItem value="psn">psn</MenuItem>
+          <MenuItem value="steam">steam</MenuItem>
+        </TextField>
+      </Grid>
 
       <Grid container>
         {buttonsGameMode.map((modeName, idx) => (
@@ -96,6 +151,15 @@ export default function LifetimeStats() {
       ) : (
         "nie udało sie"
       )}
+      <div>
+        {seasonsList &&
+          seasonsList?.map((el, idx) => {
+            return (
+            <ul key={idx}>
+              <li >{el.id}</li>
+            </ul>)
+          })}
+      </div>
     </Stack>
   );
 }
