674e1990dc3fb164e49a539d3f996d60a9e1e2d3
LeshchuDev <leshchudev@gmail.com>
2024-02-13 19:49:31 +0100

    smaller footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,12 @@ import { Stack, Typography } from "@mui/material";
 export default function Footer() {
   return (
     <footer className="footer">
-      <Stack bgcolor='grey.900' direction='row' justifyContent='space-between' sx={{width:'100vw'}}>
+      <Stack bgcolor='grey.900' direction='row' justifyContent='space-between' sx={{width:'100vw'}} spacing={1}>
         {" "}
-      <Typography fontSize='0.7em'> Pubg score -  not affiliated with Krafton,Inc. , PLAYERUNKNOWN'S BATTLEGROUNDS or PUBG
+      <Typography fontSize='0.5em'> Pubg score -  not affiliated with Krafton,Inc. , PLAYERUNKNOWN'S BATTLEGROUNDS or PUBG
         Corp</Typography>
-        <Typography fontSize='0.7em'>made with love by gamer</Typography>
-        <Typography fontSize='0.7em'>email: leshchudev@gmail.com</Typography>
+        <Typography fontSize='0.5em'>made with love by gamer</Typography>
+        <Typography fontSize='0.5em'>email: leshchudev@gmail.com</Typography>
       </Stack>
     </footer>
   );
