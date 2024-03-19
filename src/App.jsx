57a0e74b7378132d49138f1102d28ef4ecc4fe11
LeshchuDev <leshchudev@gmail.com>
2024-03-19 21:48:01 +0100

    resizeBackground switched off

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="bgd-container">
       <div className="bgd">
-        <ResizeBackground />
+        {/* <ResizeBackground /> */}
         <Container maxWidth="lg">
           <HashRouter>
             <Browser />
