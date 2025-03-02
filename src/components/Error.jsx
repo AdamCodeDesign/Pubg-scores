fea486c5c57c6a64384776869fc434bcdfffdc30
LeshchuDev <leshchudev@gmail.com>
2025-03-02 22:58:09 +0100

    error centered

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -2,13 +2,13 @@ import { Typography } from "@mui/material";
 import { useParams } from "react-router-dom";
 
 export default function Error() {
-  const { status } = useParams();
-  console.log("status",status);
+    const { status } = useParams();
+    console.log("status", status);
 
-  return (
-    <Typography variant="h4" color="red">
-      {status === "429" && "Serwer down"}
-      {status === "404" && "Player NOT FOUND"}
-    </Typography>
-  );
+    return (
+        <Typography variant="h4" color="red" style={{ margin: "0 auto" }}>
+            {status === "429" && "Serwer down"}
+            {status === "404" && "Player NOT FOUND"}
+        </Typography>
+    );
 }
